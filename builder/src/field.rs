fefbf3ebc507a7ba3bd5eb17ced1b81d310e90e0
Adam Szalkowski <a.szalkowski@datahow.ch>
2023-01-23 08:06:52 +0100

    feat: multi setters

--- builder/src/field.rs
+++ builder/src/field.rs
@@ -1,15 +1,40 @@
 use proc_macro2::Ident;
-use syn::{Field, GenericArgument, PathArguments, Type};
+use syn::{Field, GenericArgument, Lit, Meta, MetaList, MetaNameValue, Path, PathArguments, Type};
 
 pub(crate) enum BuilderField<'a> {
-    Mandatory { ident: &'a Ident, ty: &'a Type },
-    Optional { ident: &'a Ident, ty: &'a Type },
+    Mandatory {
+        ident: &'a Ident,
+        ty: &'a Type,
+    },
+    Optional {
+        ident: &'a Ident,
+        ty: &'a Type,
+    },
+    Multi {
+        ident: &'a Ident,
+        attrs: Vec<Ident>,
+        with_set_all: bool,
+        ty: &'a Type,
+        elem_ty: &'a Type,
+    },
 }
 
 impl<'a> From<&'a Field> for BuilderField<'a> {
     fn from(field: &'a Field) -> Self {
         let ident = field.ident.as_ref().unwrap();
-        if let Some(ty) = type_behind_option(&field.ty) {
+        let each_attrs = get_each_attrs(field);
+
+        if each_attrs.len() > 0 {
+            let elem_ty = type_behind_vec(&field.ty).unwrap();
+            let with_set_all = each_attrs.iter().all(|attr| attr != ident);
+            BuilderField::Multi {
+                ident,
+                with_set_all,
+                attrs: each_attrs,
+                ty: &field.ty,
+                elem_ty,
+            }
+        } else if let Some(ty) = type_behind_option(&field.ty) {
             BuilderField::Optional { ident, ty }
         } else {
             BuilderField::Mandatory {
@@ -18,6 +43,50 @@ impl<'a> From<&'a Field> for BuilderField<'a> {
             }
         }
     }
+}
+
+fn get_each_attrs(field: &Field) -> Vec<Ident> {
+    field
+        .attrs
+        .iter()
+        .flat_map(|attr| {
+            if !path_is_ident(&attr.path, "builder") {
+                return vec![];
+            }
+
+            let meta = attr.parse_meta().unwrap();
+            match meta {
+                Meta::List(MetaList { nested, .. }, ..) => nested.into_iter(),
+                _ => unimplemented!(),
+            }
+            .map(|meta| match meta {
+                syn::NestedMeta::Meta(meta) => meta,
+                _ => unimplemented!(),
+            })
+            .map(|meta| match meta {
+                Meta::NameValue(MetaNameValue { path, lit, .. })
+                    if path_is_ident(&path, "each") =>
+                {
+                    lit
+                }
+                _ => unimplemented!(),
+            })
+            .collect::<Vec<_>>()
+        })
+        .map(|lit| match lit {
+            Lit::Str(ref s) => Ident::new(&s.value(), lit.span()),
+            _ => unimplemented!(),
+        })
+        .collect()
+}
+
+fn path_is_ident(path: &Path, ident: &str) -> bool {
+    let Some(path_ident) = path
+            .get_ident() else {
+                return false;
+            };
+
+    path_ident == ident
 }
 
 fn type_behind_option(ty: &Type) -> Option<&Type> {
@@ -37,6 +106,33 @@ fn type_behind_option(ty: &Type) -> Option<&Type> {
     };
 
     if args.args.len() != 1 {
+        return None;
+    }
+
+    let GenericArgument::Type(ty) = args.args.first()? else {
+        return None;
+    };
+
+    Some(ty)
+}
+
+fn type_behind_vec(ty: &Type) -> Option<&Type> {
+    let Type::Path(syn::TypePath {
+            qself: None,
+            path: syn::Path { ref segments, .. },
+            ..
+        }) = ty else {return None};
+
+    let last_segment = segments.last()?;
+    if last_segment.ident != "Vec" {
+        return None;
+    }
+
+    let PathArguments::AngleBracketed(ref args) = last_segment.arguments else {
+        return None;
+    };
+
+    if args.args.len() < 1 {
         return None;
     }
 
--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -5,10 +5,11 @@ use syn::{DataStruct, DeriveInput, Fields};
 mod field;
 use field::BuilderField;
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse_macro_input!(input as DeriveInput);
     let builder = builder(&input);
+    eprintln!("{}", &builder);
 
     builder.into()
 }
@@ -47,6 +48,9 @@ fn builder_type(fields: &[BuilderField], builder_ident: &Ident) -> TokenStream {
         BuilderField::Mandatory { ident, ty } | BuilderField::Optional { ident, ty } => {
             quote! { #ident: Option<#ty> }
         }
+        BuilderField::Multi { ident, ty, .. } => {
+            quote! { #ident: #ty }
+        }
     });
 
     quote! {
@@ -64,7 +68,10 @@ fn builder_constructor(
 ) -> TokenStream {
     let field_initializers = fields.iter().map(|field| match *field {
         BuilderField::Mandatory { ident, .. } | BuilderField::Optional { ident, .. } => {
-            quote! { #ident: None }
+            quote! { #ident: Option::None }
+        }
+        BuilderField::Multi { ident, .. } => {
+            quote! { #ident: ::core::default::Default::default() }
         }
     });
 
@@ -81,18 +88,52 @@ fn builder_constructor(
 }
 
 fn builder_setters(fields: &[BuilderField], builder_ident: &Ident) -> TokenStream {
-    let field_setters = fields.iter().map(|field| match *field {
-        BuilderField::Mandatory { ident, ty } | BuilderField::Optional { ident, ty } => quote! {
+    let field_setters = fields.iter().flat_map(|field| match *field {
+        BuilderField::Mandatory { ident, ty } | BuilderField::Optional { ident, ty } => {
+            Some(quote! {
+                fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
+            })
+        }
+        BuilderField::Multi {
+            ident,
+            ty,
+            with_set_all,
+            ..
+        } => with_set_all.then_some(quote! {
             fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                self.#ident = Some(#ident);
+                self.#ident = #ident;
                 self
             }
-        },
+        }),
+    });
+
+    let multi_setters = fields.iter().flat_map(|field| match *field {
+        BuilderField::Multi {
+            ident,
+            elem_ty,
+            ref attrs,
+            ..
+        } => attrs
+            .iter()
+            .map(|attr| {
+                quote! {
+                    fn #attr(&mut self, #attr: #elem_ty) -> &mut Self {
+                        self.#ident.push(#attr);
+                        self
+                    }
+                }
+            })
+            .collect(),
+        _ => vec![],
     });
 
     quote! {
         impl #builder_ident {
             #(#field_setters)*
+            #(#multi_setters)*
         }
     }
     .into()
@@ -100,8 +141,8 @@ fn builder_setters(fields: &[BuilderField], builder_ident: &Ident) -> TokenStrea
 
 fn builder_method(fields: &[BuilderField], ident: &Ident, builder_ident: &Ident) -> TokenStream {
     let build_fields = fields.iter().map(|field| match *field {
-        BuilderField::Optional { ident, .. } => quote! {
-            #ident: self.#ident.take()
+        BuilderField::Optional { ident, .. } | BuilderField::Multi { ident, .. } => quote! {
+            #ident: ::std::mem::take(&mut self.#ident)
         },
         BuilderField::Mandatory { ident, .. } => {
             let msg = format!("{} not set", ident);
